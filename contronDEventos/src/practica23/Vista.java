675099da33ff363745d4bf62d987107df7f3149f
1DAW <1DAW@DESKTOP-84KDH6P>
2026-04-09 14:52:32 +0200

    cvv

--- contronDEventos/src/practica23/Vista.java
+++ contronDEventos/src/practica23/Vista.java
@@ -1,10 +1,10 @@
 package practica23;
 
-import java.awt.FlowLayout;
-import java.awt.TextField;
-import java.util.Iterator;
-
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
+
+import inmobiliaria.*;
 
 public class Vista extends JPanel{
 	
@@ -13,10 +13,22 @@ public class Vista extends JPanel{
 	 */
 	//-----DATOS GENERALES-----//
 	
-	JLabel lCodigo, lTPropiedad, lPrecio, lSuperficie, lDireccion, lDescripcion;
 	JTextField tfCodigo, tfPrecio, tfSuperficie, tfDireccion;
 	JTextArea taDescripcion;
 	JComboBox<EnumPropiedad> cbPropiedad;
+	
+	//--/////////////////////--//
+	
+	//-----DATOS VIVIENDA-----//
+
+	JComboBox<EnumViviendas> cbVivienda;
+	JComboBox<Integer> cbDormitorios, cbBaños;
+	
+	//--/////////////////////--//
+	
+	//-----DATOS FINCA-----//
+
+	JLabel lterreno, lElectrico, lAgua, lViviendaSN;
 	
 	//--/////////////////////--//
 	
@@ -26,29 +38,38 @@ public class Vista extends JPanel{
 		panelPrincipal.setLayout(new BoxLayout(panelPrincipal, BoxLayout.Y_AXIS));
 		
 		panelPrincipal.add(datosGenerales());
+		panelPrincipal.add(viviendas());
+		panelPrincipal.add(fincas());
 		
+		panelPrincipal.setPreferredSize(new Dimension(640,620));
 		this.setLayout(new FlowLayout());
 		this.add(panelPrincipal);
 	}
 	
+
 	public JPanel datosGenerales() {
-		JPanel datosG = new JPanel(new FlowLayout(FlowLayout.CENTER,10,10));
+		JPanel datosG = new JPanel(new FlowLayout(FlowLayout.LEFT,10,10));
+		datosG.setBorder(new TitledBorder("Datos generales"));
 		
-		lCodigo = new JLabel("Codigo:");
-		lTPropiedad = new JLabel("Tipo propiedad:");
-		lPrecio = new JLabel("Precio:");
-		lSuperficie = new JLabel("Superficie:");
-		lDireccion  = new JLabel("Direccion:");
-		lDescripcion = new JLabel("Descripcion:");
+		JLabel lCodigo = new JLabel("Codigo:");
+		JLabel lTPropiedad = new JLabel("Tipo propiedad:");
+		JLabel lPrecio = new JLabel("Precio:");
+		JLabel lSuperficie = new JLabel("Superficie:");
+		JLabel lDireccion  = new JLabel("Direccion:");
+		JLabel lDescripcion = new JLabel("Descripcion:");
 		
 		tfCodigo = new JTextField(3);
 		tfPrecio = new JTextField("0",8);
 		tfPrecio.setHorizontalAlignment(JTextField.RIGHT);
 		tfSuperficie = new JTextField("0",3);
 		tfSuperficie.setHorizontalAlignment(JTextField.RIGHT);
-		tfDireccion = new JTextField();
+		tfDireccion = new JTextField(38);
 		
-		taDescripcion = new JTextArea();
+		JPanel panelillo = new JPanel();
+		taDescripcion = new JTextArea(4,40);
+		JScrollPane descripcion = new JScrollPane(taDescripcion);
+		panelillo.add(lDescripcion);
+		panelillo.add(descripcion);
 		
 		cbPropiedad = new JComboBox<EnumPropiedad>(EnumPropiedad.values());
 		
@@ -63,10 +84,46 @@ public class Vista extends JPanel{
 		datosG.add(tfSuperficie);
 		datosG.add(lDireccion);
 		datosG.add(tfDireccion);
-		datosG.add(taDescripcion);
+		datosG.add(panelillo);
 		
 		return datosG;
 	}
+	
+	private Component viviendas() {
+		JPanel datosV = new JPanel(new FlowLayout(FlowLayout.CENTER,10,10));
+		datosV.setBorder(new TitledBorder("Datos especificos para vivienda"));
+
+		JLabel lNumVivienda = new JLabel("Tipo de vivienda: ");
+		JLabel lDormitorios = new JLabel("Numero de dormitorios: ");
+		JLabel lBaños = new JLabel("Numero de baños: ");
+		
+		cbVivienda = new JComboBox<EnumViviendas>(EnumViviendas.values());
+		cbVivienda.setEnabled(false);
+		
+		Integer[] arrayNumeros = {1,2,3,4,5,6,7,8,9};
+		cbDormitorios = new JComboBox<Integer>(arrayNumeros);
+		cbDormitorios.setEnabled(false);
+		cbBaños = new JComboBox<Integer>(arrayNumeros);
+		cbBaños.setEnabled(false);
+		
+		datosV.add(lNumVivienda);
+		datosV.add(cbVivienda);
+		datosV.add(lDormitorios);
+		datosV.add(cbDormitorios);
+		datosV.add(lBaños);
+		datosV.add(cbBaños);
+			
+		
+		return datosV;
+	}
+
+	private Component fincas() {
+		JPanel datosF = new JPanel(new GridLayout(4,1));
+
+		return datosF;
+	}
+	
+	
 	
 
 	public void control(Controlador ctr) {
